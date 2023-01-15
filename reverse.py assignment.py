5b0399b9ed84ed666e7900f75df78d2973043465
Kiranmai <58254196+kiranmai613@users.noreply.github.com>
2023-01-15 09:56:26 +0530

    Add files via upload
    answer 2 mirror

---
+++ reverse.py assignment.py
@@ -0,0 +1,10 @@
+# Write a Python program that accepts a word from the user and reverse it.
+
+# Sample Test Case
+# Input : Edyoda
+
+# output: adoydE
+input=str(input("enter input"))
+input=input[::-1]
+print(input)
+
